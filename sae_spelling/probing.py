c49567e3d28f69e2b2546b47f12a277c43ad4227
David Chanin <chanindav@gmail.com>
2024-07-20 12:36:08 +0100

    improving performance and tests for logistic regression classifier

--- sae_spelling/probing.py
+++ sae_spelling/probing.py
@@ -1,9 +1,13 @@
+from typing import Callable
+
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader, TensorDataset, WeightedRandomSampler
+from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
 from sae_spelling.util import DEFAULT_DEVICE
+
+EPS = 1e-8
 
 
 class LinearProbe(nn.Module):
@@ -11,55 +15,45 @@ class LinearProbe(nn.Module):
     Based on by https://github.com/jbloomAus/alphabetical_probe/blob/main/src/probes.py
     """
 
-    def __init__(self, input_dim, num_classes: int = 1):
+    def __init__(self, input_dim, num_outputs: int = 1):
         super().__init__()
-        self.fc = nn.Linear(input_dim, num_classes)
-        nn.init.xavier_uniform_(self.fc.weight)
+        self.fc = nn.Linear(input_dim, num_outputs)
 
     def forward(self, x):
         return self.fc(x)
 
-
-def get_class_weights(y, num_classes):
-    """
-    Returns a tensor of inverse class frequencies
-    """
-    return (len(y) / torch.bincount(y, minlength=num_classes)).tolist()
+    @property
+    def weights(self):
+        return self.fc.weight
 
 
-def create_rebalanced_dataloader(
-    x: torch.Tensor,
-    y: torch.Tensor,
-    num_classes: int,
-    batch_size: int = 512,
-):
-    """
-    Create a dataloader with weighted sampling based on class frequencies
-    """
-    dataset = TensorDataset(x, y)
-    class_weights = get_class_weights(y, num_classes)
-    sampler = WeightedRandomSampler([class_weights[label] for label in y], len(y))
-    return DataLoader(dataset, batch_size=batch_size, sampler=sampler)
+def _calc_pos_weights(y: torch.Tensor) -> torch.Tensor:
+    num_pos_samples = y.sum(dim=0)
+    num_neg_samples = len(y) - num_pos_samples
+    return num_neg_samples / num_pos_samples
 
 
-def train_multiclass_probe(
+def train_multi_probe(
     x_train: torch.Tensor,  # tensor of shape (num_samples, input_dim)
-    y_train: torch.Tensor,  # tensor of shape (num_samples,), with values in [0, num_classes)
-    num_classes: int | None = None,  # inferred from y_train if None
+    y_train: torch.Tensor,  # tensor of shape (num_samples, num_probes), with values in [0, 1]
+    num_probes: int | None = None,  # inferred from y_train if None
     batch_size: int = 256,
-    num_epochs: int = 5,
-    lr: float = 0.001,
+    num_epochs: int = 100,
+    lr: float = 0.01,
     weight_decay: float = 1e-7,
     show_progress: bool = True,
+    verbose: bool = False,
+    early_stopping: bool = True,
     device: torch.device = DEFAULT_DEVICE,
 ) -> LinearProbe:
     """
-    Train a logistic regression probe on the given data
+    Train a multi-class one-vs-rest logistic regression probe on the given data.
+    This is equivalent to training num_probes separate binary logistic regression probes.
 
     Args:
         x_train: tensor of shape (num_samples, input_dim)
-        y_train: tensor of shape (num_samples,), with values in [0, num_classes)
-        num_classes: number of classes in the dataset
+        y_train: one_hot (or multi-hot) tensor of shape (num_samples, num_probes), with values in [0, 1]
+        num_probes: number of probes to train simultaneously
         batch_size: batch size for training
         num_epochs: number of epochs to train for
         lr: learning rate
@@ -67,36 +61,111 @@ def train_multiclass_probe(
         show_progress: whether to show a progress bar
         device: device to train on
     """
-    num_classes = num_classes or len(torch.unique(y_train))
-    loader = create_rebalanced_dataloader(
-        x_train.to(device), y_train.to(device), num_classes, batch_size=batch_size
+    dtype = x_train.dtype
+    num_probes = num_probes or y_train.shape[-1]
+    dataset = TensorDataset(x_train.to(device), y_train.to(device, dtype=dtype))
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    probe = LinearProbe(x_train.shape[-1], num_outputs=num_probes).to(
+        device, dtype=dtype
     )
-    model = LinearProbe(x_train.shape[-1], num_classes=num_classes)
-    model.to(device)
-    model.train()
 
-    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    _run_probe_training(
+        probe,
+        loader,
+        loss_fn=nn.BCEWithLogitsLoss(pos_weight=_calc_pos_weights(y_train)),
+        num_epochs=num_epochs,
+        lr=lr,
+        weight_decay=weight_decay,
+        show_progress=show_progress,
+        verbose=verbose,
+        early_stopping=early_stopping,
+    )
+
+    return probe
+
+
+def train_binary_probe(
+    x_train: torch.Tensor,  # tensor of shape (num_samples, input_dim)
+    y_train: torch.Tensor,  # tensor of shape (num_samples,), with values in [0, 1]
+    batch_size: int = 256,
+    num_epochs: int = 100,
+    lr: float = 0.01,
+    weight_decay: float = 1e-7,
+    show_progress: bool = True,
+    verbose: bool = False,
+    early_stopping: bool = True,
+    device: torch.device = DEFAULT_DEVICE,
+) -> LinearProbe:
+    """
+    Train a logistic regression probe on the given data. This is a thin wrapped around train_multi_probe.
+
+    Args:
+        x_train: tensor of shape (num_samples, input_dim)
+        y_train: tensor of shape (num_samples,), with values in [0, 1]
+        batch_size: batch size for training
+        num_epochs: number of epochs to train for
+        lr: learning rate
+        weight_decay: weight decay
+        show_progress: whether to show a progress bar
+        device: device to train on
+    """
+    return train_multi_probe(
+        x_train,
+        y_train.unsqueeze(1),
+        num_probes=1,
+        batch_size=batch_size,
+        num_epochs=num_epochs,
+        lr=lr,
+        weight_decay=weight_decay,
+        show_progress=show_progress,
+        verbose=verbose,
+        device=device,
+        early_stopping=early_stopping,
+    )
+
+
+def _run_probe_training(
+    probe: LinearProbe,
+    loader: DataLoader,
+    loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+    num_epochs: int,
+    lr: float,
+    weight_decay: float,
+    show_progress: bool,
+    verbose: bool,
+    early_stopping: bool,
+) -> None:
+    probe.train()
+    optimizer = optim.Adam(probe.parameters(), lr=lr, weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode="min", factor=0.1, patience=10, verbose=True
+        optimizer,
+        mode="min",
+        factor=0.1,
+        patience=3,
+        eps=EPS,
     )
-    loss_fn = nn.CrossEntropyLoss()
-
     for epoch in range(num_epochs):
         pbar = tqdm(loader, disable=not show_progress)
         epoch_sum_loss = 0
         for batch_embeddings, batch_labels in pbar:
             optimizer.zero_grad()
-            logits = model(batch_embeddings)
+            logits = probe(batch_embeddings)
             loss = loss_fn(logits, batch_labels)
             loss.backward()
             optimizer.step()
             epoch_sum_loss += loss.item()
             pbar.set_description(
-                f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}"
+                f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.8f}"
             )
         epoch_mean_loss = epoch_sum_loss / len(loader)
+        last_lr = scheduler.get_last_lr()
+        if last_lr[0] <= 2 * EPS and early_stopping:
+            if verbose:
+                print("Early stopping")
+            break
+        if verbose:
+            print(
+                f"epoch {epoch} sum loss: {epoch_sum_loss:.8f}, mean loss: {epoch_mean_loss:.8f} lr: {last_lr}"
+            )
         scheduler.step(epoch_mean_loss)
-
-    model.eval()
-
-    return model
+    probe.eval()
--- tests/test_probing.py
+++ tests/test_probing.py
@@ -1,29 +1,162 @@
-from collections import Counter
-
+import pytest
 import torch
 from sklearn.linear_model import LogisticRegression
+from torch.nn.functional import cosine_similarity, one_hot
 
-from sae_spelling.probing import create_rebalanced_dataloader, train_multiclass_probe
-
-
-def test_create_rebalanced_dataloader_balances_class_frequencies():
-    x = torch.randn(10_000, 10)
-    weights = torch.tensor([1, 1, 5, 10, 1])
-    probs = weights / weights.sum()
-    y = torch.multinomial(probs, 10_000, replacement=True)
-    loader = create_rebalanced_dataloader(x, y, len(weights), batch_size=10)
-    counts = Counter()
-    for _, batch_y in loader:
-        assert len(batch_y) == 10
-        counts.update(batch_y.tolist())
-    assert counts.total() == 10_000
-    expected_proportion = 0.2  # 1 / 5
-    # we should get roughly an even distribution of classes, even though the input data is imbalanced
-    for i, count in counts.items():
-        assert abs(count / counts.total() - expected_proportion) < 0.02
+from sae_spelling.probing import (
+    _calc_pos_weights,
+    train_binary_probe,
+    train_multi_probe,
+)
 
 
-def test_train_multiclass_probe_scores_highly_on_fully_separable_datasets():
+def test_calc_pos_weights_returns_1_for_equal_weights():
+    y_train = torch.cat([torch.ones(5), torch.zeros(5)]).unsqueeze(1)
+    pos_weights = _calc_pos_weights(y_train)
+    assert torch.allclose(pos_weights, torch.tensor([1.0]))
+
+
+def test_calc_pos_weights_is_above_1_if_the_are_more_neg_than_pos_samples():
+    y_train = torch.cat([torch.ones(5), torch.zeros(15)]).unsqueeze(1)
+    pos_weights = _calc_pos_weights(y_train)
+    assert torch.allclose(pos_weights, torch.tensor([3.0]))
+
+
+def test_calc_pos_weights_is_below_1_if_the_are_more_neg_than_pos_samples():
+    y_train = torch.cat([torch.ones(20), torch.zeros(5)]).unsqueeze(1)
+    pos_weights = _calc_pos_weights(y_train)
+    assert torch.allclose(pos_weights, torch.tensor([0.25]))
+
+
+def test_calc_pos_weights_handles_multiclass_samples():
+    y_train = torch.tensor(
+        [
+            [0, 1],
+            [0, 1],
+            [1, 1],
+            [1, 1],
+            [0, 1],
+            [1, 0],
+        ]
+    )
+    pos_weights = _calc_pos_weights(y_train)
+    assert torch.allclose(pos_weights, torch.tensor([1.0, 0.2]))
+
+
+@pytest.mark.parametrize("seed", range(5))
+def test_train_binary_probe_scores_highly_on_fully_separable_datasets(seed):
+    torch.manual_seed(seed)
+    neg_center = 5.0 * torch.ones(64)
+    pos_center = -5.0 * torch.ones(64)
+
+    neg_xs = neg_center + torch.randn(1024, 64)
+    pos_xs = pos_center + torch.randn(368, 64)
+
+    x = torch.cat([neg_xs, pos_xs])
+    y = torch.cat([torch.zeros(len(neg_xs)), torch.ones(len(pos_xs))])
+
+    # shuffle x and y in the same way
+    perm = torch.randperm(len(x))
+    x = x[perm]
+    y = y[perm]
+
+    x_train = x[:768]
+    y_train = y[:768]
+
+    x_test = x[768:]
+    y_test = y[768:]
+
+    probe = train_binary_probe(
+        x_train,
+        y_train,
+        num_epochs=100,
+        batch_size=128,
+        weight_decay=1e-7,
+        lr=1.0,
+        show_progress=False,
+    )
+
+    train_preds = probe(x_train)[:, 0] > 0
+    test_preds = probe(x_test)[:, 0] > 0
+
+    train_acc = (train_preds == y_train).float().mean().item()
+    test_acc = (test_preds == y_test).float().mean().item()
+
+    assert train_acc > 0.98
+    assert test_acc > 0.98
+
+    sk_probe = LogisticRegression(max_iter=100, class_weight="balanced").fit(
+        x_train.numpy(), y_train.numpy()
+    )
+
+    # since this is a synthetic dataset, we know the correct direction we should learn
+    correct_dir = (pos_center - neg_center).unsqueeze(0)
+    # just verify that sklearn does get the right answer
+    sk_cos_sim = cosine_similarity(correct_dir, torch.tensor(sk_probe.coef_), dim=1)
+    assert sk_cos_sim.min().item() > 0.98
+    cos_sim = cosine_similarity(correct_dir, probe.weights, dim=1)
+    assert cos_sim.min().item() > 0.98
+
+
+@pytest.mark.parametrize("seed", range(5))
+def test_train_binary_probe_scores_highly_on_noisy_datasets(seed):
+    torch.manual_seed(seed)
+    neg_center = 1.0 * torch.ones(64)
+    pos_center = -1.0 * torch.ones(64)
+
+    neg_xs = neg_center + 3 * torch.randn(1024, 64)
+    pos_xs = pos_center + 3 * torch.randn(368, 64)
+
+    x = torch.cat([neg_xs, pos_xs])
+    y = torch.cat([torch.zeros(len(neg_xs)), torch.ones(len(pos_xs))])
+
+    # shuffle x and y in the same way
+    perm = torch.randperm(len(x))
+    x = x[perm]
+    y = y[perm]
+
+    x_train = x[:600]
+    y_train = y[:600]
+
+    x_test = x[600:]
+    y_test = y[600:]
+
+    probe = train_binary_probe(
+        x_train,
+        y_train,
+        num_epochs=100,
+        batch_size=128,
+        weight_decay=1e-7,
+        lr=1.0,
+        show_progress=False,
+    )
+
+    train_preds = probe(x_train)[:, 0] > 0
+    test_preds = probe(x_test)[:, 0] > 0
+
+    train_acc = (train_preds == y_train).float().mean().item()
+    test_acc = (test_preds == y_test).float().mean().item()
+
+    assert train_acc > 0.9
+    assert test_acc > 0.9
+
+    sk_probe = LogisticRegression(max_iter=100, class_weight="balanced").fit(
+        x_train.numpy(), y_train.numpy()
+    )
+
+    # since this is a synthetic dataset, we know the correct direction we should learn
+    correct_dir = (pos_center - neg_center).unsqueeze(0)
+    # just verify that sklearn does get the right answer
+    sk_cos_sim = cosine_similarity(correct_dir, torch.tensor(sk_probe.coef_), dim=1)
+    print(f"sklearn cos sim: {sk_cos_sim}")
+    assert sk_cos_sim.min().item() > 0.85
+    cos_sim = cosine_similarity(correct_dir, probe.weights, dim=1)
+    assert cos_sim.min().item() > 0.85
+
+
+@pytest.mark.parametrize("seed", range(5))
+def test_train_multi_probe_scores_highly_on_fully_separable_datasets(seed):
+    torch.manual_seed(seed)
     class1_center = 10 * torch.randn(64)
     class2_center = 10 * torch.randn(64)
     class3_center = 10 * torch.randn(64)
@@ -33,13 +166,15 @@ def test_train_multiclass_probe_scores_highly_on_fully_separable_datasets():
     class3_xs = class3_center + torch.randn(200, 64)
 
     x = torch.cat([class1_xs, class2_xs, class3_xs])
-    y = torch.cat(
-        [
-            0 * torch.ones(len(class1_xs)),  # 0
-            1 * torch.ones(len(class2_xs)),  # 1
-            2 * torch.ones(len(class3_xs)),  # 2
-        ]
-    ).long()
+    y = one_hot(
+        torch.cat(
+            [
+                0 * torch.ones(len(class1_xs)),  # 0
+                1 * torch.ones(len(class2_xs)),  # 1
+                2 * torch.ones(len(class3_xs)),  # 2
+            ]
+        ).long()
+    )
 
     # shuffle x and y in the same way
     perm = torch.randperm(len(x))
@@ -52,12 +187,12 @@ def test_train_multiclass_probe_scores_highly_on_fully_separable_datasets():
     x_test = x[300:]
     y_test = y[300:]
 
-    probe = train_multiclass_probe(
-        x_train, y_train, num_classes=3, num_epochs=5, batch_size=32
+    probe = train_multi_probe(
+        x_train, y_train, num_probes=3, num_epochs=100, batch_size=32
     )
 
-    train_preds = probe(x_train).argmax(dim=1)
-    test_preds = probe(x_test).argmax(dim=1)
+    train_preds = probe(x_train) > 0
+    test_preds = probe(x_test) > 0
 
     train_acc = (train_preds == y_train).float().mean().item()
     test_acc = (test_preds == y_test).float().mean().item()
@@ -66,51 +201,48 @@ def test_train_multiclass_probe_scores_highly_on_fully_separable_datasets():
     assert test_acc > 0.98
 
 
-def test_train_multiclass_probe_scores_similarly_to_sklearn_on_noisy_datasets():
-    class1_center = 0.1 * torch.randn(64)
-    class2_center = 0.1 * torch.randn(64)
-    class3_center = 0.1 * torch.randn(64)
+@pytest.mark.parametrize("seed", range(5))
+def test_train_multi_probe_scores_highly_on_noisy_datasets(seed):
+    torch.manual_seed(seed)
+    class1_center = 0.5 * torch.randn(64)
+    class2_center = 0.5 * torch.randn(64)
+    class3_center = 0.5 * torch.randn(64)
 
     class1_xs = class1_center + torch.randn(150, 64)
-    class2_xs = class2_center + torch.randn(60, 64)
+    class2_xs = class2_center + torch.randn(600, 64)
     class3_xs = class3_center + torch.randn(250, 64)
 
     x = torch.cat([class1_xs, class2_xs, class3_xs])
-    y = torch.cat(
-        [
-            0 * torch.ones(len(class1_xs)),  # 0
-            1 * torch.ones(len(class2_xs)),  # 1
-            2 * torch.ones(len(class3_xs)),  # 2
-        ]
-    ).long()
+    y = one_hot(
+        torch.cat(
+            [
+                0 * torch.ones(len(class1_xs)),  # 0
+                1 * torch.ones(len(class2_xs)),  # 1
+                2 * torch.ones(len(class3_xs)),  # 2
+            ]
+        ).long()
+    )
 
     # shuffle x and y in the same way
     perm = torch.randperm(len(x))
     x = x[perm]
     y = y[perm]
 
-    x_train = x[:300]
-    y_train = y[:300]
+    x_train = x[:500]
+    y_train = y[:500]
 
-    x_test = x[300:]
-    y_test = y[300:]
+    x_test = x[500:]
+    y_test = y[500:]
 
-    probe = train_multiclass_probe(
-        x_train,
-        y_train,
-        num_classes=3,
-        num_epochs=200,
-        batch_size=16,
-        weight_decay=1e-7,
-    )
-    sk_probe = LogisticRegression(max_iter=200, class_weight="balanced").fit(
-        x_train.numpy(), y_train.numpy()
+    probe = train_multi_probe(
+        x_train, y_train, num_probes=3, num_epochs=100, batch_size=128
     )
 
-    test_preds = probe(x_test).argmax(dim=1)
+    train_preds = probe(x_train) > 0
+    test_preds = probe(x_test) > 0
+
+    train_acc = (train_preds == y_train).float().mean().item()
     test_acc = (test_preds == y_test).float().mean().item()
 
-    sk_test_preds = torch.tensor(sk_probe.predict(x_test.numpy()))
-    sk_test_acc = (sk_test_preds == y_test).float().mean().item()
-
-    assert sk_test_acc - test_acc < 0.05
+    assert train_acc > 0.9
+    assert test_acc > 0.9
