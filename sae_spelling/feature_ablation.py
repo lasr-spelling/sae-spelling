cb151335b4e205f9a9fa957fca5485aa61e9def2
David Chanin <chanindav@gmail.com>
2024-08-05 15:16:02 +0100

    adding a feature_ablation function

---
+++ sae_spelling/feature_ablation.py
@@ -0,0 +1,85 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Sequence
+
+import torch
+from sae_lens import SAE
+from transformer_lens import HookedTransformer
+from transformer_lens.hook_points import HookPoint
+
+from sae_spelling.sae_utils import (
+    SaeReconstructionCache,
+    apply_saes_and_run,
+)
+from sae_spelling.util import batchify
+
+
+@dataclass
+class FeatureAblationsOutput:
+    sae_cache: SaeReconstructionCache
+    ablation_scores: dict[int, float]
+    original_score: float
+
+
+@torch.no_grad()
+def calculate_individual_feature_ablations(
+    model: HookedTransformer,
+    input: str,
+    metric_fn: Callable[[torch.Tensor], torch.Tensor],
+    sae: SAE,
+    ablate_features: Sequence[int],
+    ablate_token_index: int,  # TODO: this can be extended to multiple tokens
+    return_logits: bool = True,
+    batch_size: int = 10,
+    show_progress: bool = True,
+    # TODO: support not including the error term
+) -> FeatureAblationsOutput:
+    """
+    Calculate the effect of ablating each feature individually. This means return the change in metric if the
+    SAE feature never fired at all during the forward pass on the input. This is what feature attribution is approximating.
+    If we're unsure about how accurate attribution is, we can compare it to this function. This function is much slower, however.
+
+    Args:
+        model: The model to run the forward pass on.
+        input: The input to the model.
+        metric_fn: A function that takes the model output and returns a scalar metric.
+        sae: The SAE to use for ablation.
+        ablate_features: The indices of the features to ablate.
+        ablate_token_index: The index of the token to ablate.
+        return_logits: Whether to return the logits or the loss.
+        batch_size: The batch size to use for ablation.
+        show_progress: Whether to show a progress bar.
+    """
+    input_toks = model.to_tokens(input)
+    hook_point = sae.cfg.hook_name
+    original_output = apply_saes_and_run(
+        model,
+        saes={hook_point: sae},
+        input=input_toks,
+        return_type="logits" if return_logits else "loss",
+        include_error_term=True,
+    )
+    original_score = metric_fn(original_output.model_output)
+    ablation_scores = {}
+    for batch in batchify(ablate_features, batch_size, show_progress=show_progress):
+        feature_vals = original_output.sae_activations[hook_point].feature_acts[
+            0, ablate_token_index, batch
+        ]
+        batch_deltas = -1 * feature_vals.unsqueeze(-1) * sae.W_dec[batch]
+
+        def ablation_hook(value: torch.Tensor, hook: HookPoint):  # noqa: ARG001
+            value[:, ablate_token_index, :] += batch_deltas
+            return value
+
+        outputs = model.run_with_hooks(
+            input_toks.repeat(len(batch), 1),
+            fwd_hooks=[(hook_point, ablation_hook)],
+        )
+        for feat_idx, output in zip(batch, outputs):
+            score = metric_fn(output.unsqueeze(0)).item()
+            ablation_scores[feat_idx] = score - original_score
+    return FeatureAblationsOutput(
+        sae_cache=original_output.sae_activations[hook_point],
+        ablation_scores=ablation_scores,
+        original_score=original_score.item(),
+    )
