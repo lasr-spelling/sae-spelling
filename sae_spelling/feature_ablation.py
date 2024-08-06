87dcebdc1a1289e488467af3a778841cd3c30824
David Chanin <chanindav@gmail.com>
2024-08-06 05:09:11 -0600

    Feature ablation (#16)
    * adding a feature_ablation function
    
    * by default ablate every firing feature
    
    * fixing outputs to make everything floats
    
    * don't show progress by default

---
+++ sae_spelling/feature_ablation.py
@@ -0,0 +1,95 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Sequence
+
+import torch
+from sae_lens import SAE
+from transformer_lens import HookedTransformer
+from transformer_lens.hook_points import HookPoint
+
+from sae_spelling.sae_utils import (
+    SaeReconstructionCache,
+    apply_saes_and_run,
+)
+from sae_spelling.util import batchify
+
+
+@dataclass
+class FeatureAblationsOutput:
+    sae_cache: SaeReconstructionCache
+    ablation_scores: dict[int, float]
+    original_score: float
+
+
+@torch.no_grad()
+def calculate_individual_feature_ablations(
+    model: HookedTransformer,
+    input: str,
+    metric_fn: Callable[[torch.Tensor], torch.Tensor],
+    sae: SAE,
+    ablate_token_index: int,  # TODO: this can be extended to multiple tokens
+    ablate_features: Sequence[int] | None = None,
+    return_logits: bool = True,
+    batch_size: int = 25,
+    show_progress: bool = False,
+    # TODO: support not including the error term
+) -> FeatureAblationsOutput:
+    """
+    Calculate the effect of ablating each feature individually. This means return the change in metric if the
+    SAE feature never fired at all during the forward pass on the input.
+    This is what feature attribution is approximating, but the signs are flipped since attribution asks "How important is a feature?",
+    while ablation asks "what is the metric delta if the feature is deleted?".
+    If we're unsure about how accurate attribution is, we can compare it to this function (after flipping the sign). This function is much slower, however.
+
+    Args:
+        model: The model to run the forward pass on.
+        input: The input to the model.
+        metric_fn: A function that takes the model output and returns a scalar metric.
+        sae: The SAE to use for ablation.
+        ablate_features: The indices of the features to ablate. If empty, ablate all firing features.
+        ablate_token_index: The index of the token to ablate.
+        return_logits: Whether to return the logits or the loss.
+        batch_size: The batch size to use for ablation.
+        show_progress: Whether to show a progress bar.
+    """
+    input_toks = model.to_tokens(input)
+    hook_point = sae.cfg.hook_name
+    original_output = apply_saes_and_run(
+        model,
+        saes={hook_point: sae},
+        input=input_toks,
+        return_type="logits" if return_logits else "loss",
+        include_error_term=True,
+    )
+    original_score = metric_fn(original_output.model_output).item()
+    # if we didn't pass specific features to ablate, try ablating every SAE feature that fired
+    if ablate_features is None:
+        sae_acts = original_output.sae_activations[hook_point]
+        ablate_features = (
+            torch.nonzero(sae_acts.feature_acts[0, ablate_token_index])
+            .squeeze(-1)
+            .tolist()
+        )
+    ablation_scores = {}
+    for batch in batchify(ablate_features, batch_size, show_progress=show_progress):
+        feature_vals = original_output.sae_activations[hook_point].feature_acts[
+            0, ablate_token_index, batch
+        ]
+        batch_deltas = -1 * feature_vals.unsqueeze(-1) * sae.W_dec[batch]
+
+        def ablation_hook(value: torch.Tensor, hook: HookPoint):  # noqa: ARG001
+            value[:, ablate_token_index, :] += batch_deltas
+            return value
+
+        outputs = model.run_with_hooks(
+            input_toks.repeat(len(batch), 1),
+            fwd_hooks=[(hook_point, ablation_hook)],
+        )
+        for feat_idx, output in zip(batch, outputs):
+            score = metric_fn(output.unsqueeze(0)).item()
+            ablation_scores[feat_idx] = score - original_score
+    return FeatureAblationsOutput(
+        sae_cache=original_output.sae_activations[hook_point],
+        ablation_scores=ablation_scores,
+        original_score=original_score,
+    )
---
+++ tests/test_feature_ablation.py
@@ -0,0 +1,124 @@
+from pytest import approx
+from sae_lens import SAE
+from torch import Tensor
+from transformer_lens import HookedTransformer
+
+from sae_spelling.feature_ablation import calculate_individual_feature_ablations
+
+
+def test_calculate_individual_feature_ablations_returns_0_if_feat_didnt_fire(
+    gpt2_model: HookedTransformer, gpt2_l4_sae: SAE
+):
+    assert gpt2_model.tokenizer is not None
+    mary_token = gpt2_model.tokenizer.encode(" Mary")[0]
+
+    def metric_fn(logits: Tensor) -> Tensor:
+        return logits[-1, -1, mary_token]
+
+    output = calculate_individual_feature_ablations(
+        gpt2_model,
+        "When John and Mary went to the shops, John gave the bag to",
+        metric_fn=metric_fn,
+        sae=gpt2_l4_sae,
+        ablate_features=[0, 10],
+        ablate_token_index=10,
+        batch_size=10,
+    )
+    assert output.ablation_scores[0] == approx(0.0, abs=1e-5)
+    assert output.ablation_scores[10] == approx(0.0, abs=1e-5)
+
+
+def test_calculate_individual_feature_ablations_has_non_zero_vals_for_firing_features(
+    gpt2_model: HookedTransformer, gpt2_l4_sae: SAE
+):
+    assert gpt2_model.tokenizer is not None
+    mary_token = gpt2_model.tokenizer.encode(" Mary")[0]
+
+    def metric_fn(logits: Tensor) -> Tensor:
+        return logits[-1, -1, mary_token]
+
+    # ablate "John" feature: https://www.neuronpedia.org/gpt2-small/4-res-jb/1362
+    # 1024 is another random feature that activates, but less strongly
+    output = calculate_individual_feature_ablations(
+        gpt2_model,
+        "When John and Mary went to the shops, John gave the bag to",
+        metric_fn=metric_fn,
+        sae=gpt2_l4_sae,
+        ablate_features=[1362, 1024, 10],
+        ablate_token_index=-5,  # second John index
+        batch_size=10,
+    )
+    # just ensure we've outputting something for the original metric
+    assert abs(output.original_score) > 0.1
+    assert isinstance(output.original_score, float)
+    # 1362 should drop the metric by a lot more than 1024 should do anything
+    assert output.ablation_scores[1362] < -0.9
+    assert isinstance(output.ablation_scores[1362], float)
+    assert abs(output.ablation_scores[1024]) < 0.2
+    assert output.ablation_scores[10] == approx(0.0, abs=1e-5)
+
+
+def test_calculate_individual_feature_ablations_ablates_all_firing_features_by_default(
+    gpt2_model: HookedTransformer, gpt2_l4_sae: SAE
+):
+    assert gpt2_model.tokenizer is not None
+    mary_token = gpt2_model.tokenizer.encode(" Mary")[0]
+
+    def metric_fn(logits: Tensor) -> Tensor:
+        return logits[-1, -1, mary_token]
+
+    # ablate "John" feature: https://www.neuronpedia.org/gpt2-small/4-res-jb/1362
+    # 1024 is another random feature that activates, but less strongly
+    output = calculate_individual_feature_ablations(
+        gpt2_model,
+        "When John and Mary went to the shops, John gave the bag to",
+        metric_fn=metric_fn,
+        sae=gpt2_l4_sae,
+        ablate_token_index=-5,  # second John index
+        batch_size=10,
+    )
+    assert 1362 in output.ablation_scores
+    assert 1024 in output.ablation_scores
+    assert 10 not in output.ablation_scores
+    firing_features = (
+        output.sae_cache.feature_acts[0, -5, :].nonzero().squeeze().tolist()
+    )
+    assert output.ablation_scores.keys() == set(firing_features)
+
+
+def test_calculate_individual_feature_ablations_gives_same_results_regardless_of_batch_size(
+    gpt2_model: HookedTransformer, gpt2_l4_sae: SAE
+):
+    assert gpt2_model.tokenizer is not None
+    mary_token = gpt2_model.tokenizer.encode(" Mary")[0]
+
+    def metric_fn(logits: Tensor) -> Tensor:
+        return logits[-1, -1, mary_token]
+
+    output1 = calculate_individual_feature_ablations(
+        gpt2_model,
+        "When John and Mary went to the shops, John gave the bag to",
+        metric_fn=metric_fn,
+        sae=gpt2_l4_sae,
+        ablate_features=[1362, 1024, 10],
+        ablate_token_index=-5,  # second John index
+        batch_size=10,
+    )
+    output2 = calculate_individual_feature_ablations(
+        gpt2_model,
+        "When John and Mary went to the shops, John gave the bag to",
+        metric_fn=metric_fn,
+        sae=gpt2_l4_sae,
+        ablate_features=[1362, 1024, 10],
+        ablate_token_index=-5,  # second John index
+        batch_size=1,
+    )
+    assert output1.ablation_scores[1362] - output2.ablation_scores[1362] == approx(
+        0.0, abs=1e-5
+    )
+    assert output1.ablation_scores[1024] - output2.ablation_scores[1024] == approx(
+        0.0, abs=1e-5
+    )
+    assert output1.ablation_scores[10] - output2.ablation_scores[10] == approx(
+        0.0, abs=1e-5
+    )
