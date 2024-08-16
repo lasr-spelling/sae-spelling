daf820b7d8312ddd908a7fc191bd76c42b104ee9
David Chanin <chanindav@gmail.com>
2024-08-16 12:08:45 +0100

    adding auroc/f1 experiments

---
+++ tests/experiments/test_common.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import torch
+from syrupy.assertion import SnapshotAssertion
+from transformers import PreTrainedTokenizerFast
+
+from sae_spelling.experiments.common import (
+    SaeInfo,
+    _parse_probe_data_split,
+    get_gemmascope_saes_info,
+)
+
+
+def test_get_gemmascope_saes_info(snapshot: SnapshotAssertion):
+    saes = get_gemmascope_saes_info()
+    assert saes == snapshot
+
+
+def test_get_gemmascope_saes_info_at_specific_layer():
+    l1_saes = get_gemmascope_saes_info(layer=1)
+    assert l1_saes == [
+        SaeInfo(l0=10, layer=1, width=16_000, path="layer_1/width_16k/average_l0_10"),
+        SaeInfo(l0=102, layer=1, width=16_000, path="layer_1/width_16k/average_l0_102"),
+        SaeInfo(l0=20, layer=1, width=16_000, path="layer_1/width_16k/average_l0_20"),
+        SaeInfo(l0=250, layer=1, width=16_000, path="layer_1/width_16k/average_l0_250"),
+        SaeInfo(l0=40, layer=1, width=16_000, path="layer_1/width_16k/average_l0_40"),
+        SaeInfo(l0=121, layer=1, width=65_000, path="layer_1/width_65k/average_l0_121"),
+        SaeInfo(l0=16, layer=1, width=65_000, path="layer_1/width_65k/average_l0_16"),
+        SaeInfo(l0=30, layer=1, width=65_000, path="layer_1/width_65k/average_l0_30"),
+        SaeInfo(l0=54, layer=1, width=65_000, path="layer_1/width_65k/average_l0_54"),
+        SaeInfo(l0=9, layer=1, width=65_000, path="layer_1/width_65k/average_l0_9"),
+    ]
+
+
+def test_parse_probe_data_split(gpt2_tokenizer: PreTrainedTokenizerFast):
+    split_activations = torch.randn(4, 12)
+    split_labels = [5, 1, 18, 22]
+    split_indices = [2, 3, 4, 5]
+    df = pd.DataFrame(
+        {
+            "token": ["dog", "cat", "fish", "bird", "shark", "whale", "dolphin"],
+        }
+    )
+    activations, vocab_with_labels = _parse_probe_data_split(
+        gpt2_tokenizer, split_activations, split_labels, split_indices, df
+    )
+    assert torch.allclose(activations, split_activations)
+    assert vocab_with_labels == [
+        ("fish", 5),
+        ("bird", 1),
+        ("shark", 18),
+        ("whale", 22),
+    ]
+
+
+def test_parse_probe_data_split_removes_invalid_rows(
+    gpt2_tokenizer: PreTrainedTokenizerFast,
+):
+    split_activations = torch.randn(5, 12)
+    split_labels = [5, 1, 18, 22, 23]
+    split_indices = [2, 3, 4, 5, 6]
+    df = pd.DataFrame(
+        {
+            "token": [
+                "dog",
+                "cat",
+                "fish",
+                "bird",
+                float("nan"),
+                "whale",
+                "<0x6A>",
+                "dolphin",
+            ],
+        }
+    )
+    activations, vocab_with_labels = _parse_probe_data_split(
+        gpt2_tokenizer, split_activations, split_labels, split_indices, df
+    )
+    assert torch.allclose(activations, split_activations[[0, 1, 3]])
+    assert vocab_with_labels == [
+        ("fish", 5),
+        ("bird", 1),
+        ("whale", 22),
+    ]
+
+
+def test_parse_probe_data_split_replaces_special_token_chars(
+    gpt2_tokenizer: PreTrainedTokenizerFast,
+):
+    split_activations = torch.randn(2, 12)
+    split_labels = [18, 22]
+    split_indices = [1, 2]
+    df = pd.DataFrame(
+        {
+            "token": [
+                "bird",
+                "Ġsculpt",
+                "whale",
+            ],
+        }
+    )
+    activations, vocab_with_labels = _parse_probe_data_split(
+        gpt2_tokenizer, split_activations, split_labels, split_indices, df
+    )
+    assert torch.allclose(activations, split_activations)
+    assert vocab_with_labels == [
+        (" sculpt", 18),
+        ("whale", 22),
+    ]
---
+++ tests/experiments/test_encoder_auroc_and_f1.py
@@ -0,0 +1,29 @@
+import torch
+from sae_lens import SAE
+from syrupy.assertion import SnapshotAssertion
+
+from sae_spelling.experiments.encoder_auroc_and_f1 import (
+    eval_probe_and_top_sae_raw_scores,
+)
+from sae_spelling.probing import LinearProbe
+
+
+@torch.no_grad()
+def test_eval_probe_and_top_sae_raw_scores_gives_sane_results(
+    gpt2_l4_sae: SAE, snapshot: SnapshotAssertion
+):
+    fake_probe = LinearProbe(768, 26)
+    fake_probe.weights[2, :] = gpt2_l4_sae.W_enc[:, 123]  # set C to feature 123
+
+    eval_data = [("dog", 3), ("cat", 2), ("fish", 5), ("bird", 1)]
+    eval_activations = torch.randn(4, 768)
+    df = eval_probe_and_top_sae_raw_scores(
+        gpt2_l4_sae,
+        fake_probe,
+        eval_data,
+        eval_activations,
+        metadata={"layer": 4},
+        topk=2,
+    )
+    assert all(df["sae_c_top_0_feat"] == 123)
+    assert df.columns.values.tolist() == snapshot
