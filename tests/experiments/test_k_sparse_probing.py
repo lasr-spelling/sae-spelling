f0a415608bd0c5825d108547346f33ed4dd133e1
David Chanin <chanindav@gmail.com>
2024-09-15 23:17:39 +0100

    more lenient test checking

--- tests/experiments/test_k_sparse_probing.py
+++ tests/experiments/test_k_sparse_probing.py
@@ -54,7 +54,7 @@ def test_get_sae_acts_gives_same_results_batched_and_not_batched(gpt2_l4_sae: SA
     token_acts = torch.randn(10, 768)
     sae_acts_unbatched = _get_sae_acts(gpt2_l4_sae, token_acts, True, batch_size=1)
     sae_acts_batched = _get_sae_acts(gpt2_l4_sae, token_acts, True, batch_size=5)
-    assert torch.allclose(sae_acts_unbatched, sae_acts_batched)
+    assert torch.allclose(sae_acts_unbatched, sae_acts_batched, atol=1e-3)
 
 
 def test_eval_probe_and_sae_k_sparse_raw_scores_gives_sane_results(
